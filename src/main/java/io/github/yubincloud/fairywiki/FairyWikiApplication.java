35dc3cb6e387cff22de078a3d4e12ec98dd63a27
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-14 13:55:44 +0800

    init springboot project

---
+++ src/main/java/io/github/yubincloud/fairywiki/FairyWikiApplication.java
@@ -0,0 +1,13 @@
+package io.github.yubincloud.fairywiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairyWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairyWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/yubincloud/fairywiki/FairyWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yubincloud.fairywiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairyWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
