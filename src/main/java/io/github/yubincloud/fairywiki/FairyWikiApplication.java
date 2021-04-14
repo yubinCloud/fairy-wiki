35ce2af4cae54503f46b0067325bbf36a9bb219a
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-14 14:27:23 +0800

    2-5 增加了启动日志的定制化

--- src/main/java/io/github/yubincloud/fairywiki/FairyWikiApplication.java
+++ src/main/java/io/github/yubincloud/fairywiki/FairyWikiApplication.java
@@ -1,13 +1,20 @@
 package io.github.yubincloud.fairywiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class FairyWikiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FairyWikiApplication.class, args);
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(FairyWikiApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(FairyWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
 }
