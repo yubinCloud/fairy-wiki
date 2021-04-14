99550b6117a5eefdec580f3fde868d70974632e6
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-14 15:03:30 +0800

    2-7 增加了专门用于 http 测试的包 /http-requests，并完成了第一个对 hello 接口的测试文件

--- src/main/java/io/github/yubincloud/fairywiki/controller/TestController.java
+++ src/main/java/io/github/yubincloud/fairywiki/controller/TestController.java
@@ -1,9 +1,11 @@
 package io.github.yubincloud.fairywiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/hello")
