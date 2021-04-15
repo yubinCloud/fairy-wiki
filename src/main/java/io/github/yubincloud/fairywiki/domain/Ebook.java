5b7f1ce65629c18c0653f18321d7dfd0364d0f11
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-15 09:29:24 +0800

    3-6 增加了 ebook 数据库表以及相应的 mybatis 持久层

---
+++ src/main/java/io/github/yubincloud/fairywiki/domain/Ebook.java
@@ -0,0 +1,110 @@
+package io.github.yubincloud.fairywiki.domain;
+
+public class Ebook {
+    private Long id;
+
+    private String name;
+
+    private Long category1Id;
+
+    private Long category2Id;
+
+    private String description;
+
+    private String cover;
+
+    private Integer docCount;
+
+    private Integer viewCount;
+
+    private Integer voteCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCategory1Id() {
+        return category1Id;
+    }
+
+    public void setCategory1Id(Long category1Id) {
+        this.category1Id = category1Id;
+    }
+
+    public Long getCategory2Id() {
+        return category2Id;
+    }
+
+    public void setCategory2Id(Long category2Id) {
+        this.category2Id = category2Id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Integer getDocCount() {
+        return docCount;
+    }
+
+    public void setDocCount(Integer docCount) {
+        this.docCount = docCount;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() +
+                " [" +
+                "Hash = " + hashCode() +
+                ", id=" + id +
+                ", name=" + name +
+                ", category1Id=" + category1Id +
+                ", category2Id=" + category2Id +
+                ", description=" + description +
+                ", cover=" + cover +
+                ", docCount=" + docCount +
+                ", viewCount=" + viewCount +
+                ", voteCount=" + voteCount +
+                "]";
+    }
+}
---
+++ src/main/java/io/github/yubincloud/fairywiki/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package io.github.yubincloud.fairywiki.mapper;
+
+import io.github.yubincloud.fairywiki.domain.Ebook;
+import io.github.yubincloud.fairywiki.domain.EbookExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
