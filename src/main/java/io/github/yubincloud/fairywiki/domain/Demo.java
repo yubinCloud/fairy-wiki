6636f9c2e1bf8841628270dd4548011f3867fa93
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-15 09:08:42 +0800

    3-5 完成了 mybatis 代码生成器，可以通过 maven 插件实现对mysql数据库表基本操作代码的一键生成

---
+++ src/main/java/io/github/yubincloud/fairywiki/domain/Demo.java
@@ -0,0 +1,35 @@
+package io.github.yubincloud.fairywiki.domain;
+
+public class Demo {
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
+    }
+}
