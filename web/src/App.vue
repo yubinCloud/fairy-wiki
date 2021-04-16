a9216e57d09254be6e6b039c066412f8bdf7c6dd
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-16 17:30:15 +0800

    4-7 将公共的尾部也封装为一个组件 <the-footer> 并放在 App.vue 中

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <a-layout>
-    <the-header></the-header>
-    <router-view/>
-    <a-layout-footer style="text-align: center">
-      Ant Design ©2018 Created by Ant UED
-    </a-layout-footer>
+    <the-header></the-header>  <!-- 公共不变的头部-->
+    <router-view/>  <!-- 中间的内容部分，是变化的，用一个该标签作为占位符-->
+    <the-footer></the-footer>  <!-- 公共不变的尾部-->
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter
   },
 });
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Ant Design ©2018 Created by Ant UED
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'the-footer'
+})
+
+</script>
+
+<style scoped>
+
+</style>
