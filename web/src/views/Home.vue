29be162f574c85aeb16a592bbb9db034061e5d1f
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-16 22:58:35 +0800

    5-3 通过 vue 的 ref() 函数实现将请求后端得到的数据绑定到显示的页面上

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,22 +48,35 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <pre>
+        {{ ebooks }}
+      </pre>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
   setup() {
     console.log("setup");
-    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
-      console.log(response);
+    const ebooks = ref();
+
+    onMounted(() => {
+      console.log("onMounted");
+      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+        console.log(response);
+        const respData = response.data;
+        ebooks.value = respData.data;
+      })
     })
+
+    return {
+      ebooks
+    }
   }
 });
 </script>
