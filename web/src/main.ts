9f4590ab56117dcfcd75d3a8a1872126969b67b1
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-16 23:55:39 +0800

    5-4 集成了 Ant Design 的图标库

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+// 全局使用 Ant Design 的图标库
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
