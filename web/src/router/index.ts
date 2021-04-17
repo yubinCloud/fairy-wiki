6c2d36a95fc2b7df80e45714978757ef6d4def09
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-17 17:20:48 +0800

    6-2 将页面改为小写字母开头，风格如：page.vue, my-page.vue

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
+import About from '../views/about.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +11,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+    component: About
   }
 ]
 
