28847598e8b6be2310c24c35d5571c047b970cf2
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-17 14:57:04 +0800

    5-5 Vue 多环境配置，增加 dev 和 prod 环境配置文件 编译和启动命令增加多环境

--- web/src/App.vue
+++ web/src/App.vue
@@ -20,6 +20,3 @@ export default defineComponent({
 });
 </script>
 
-<style>
-
-</style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -14,3 +14,6 @@ const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+
+console.log('环境:', process.env.NODE_ENV)
