153aff1e865dbc5c844164fc8c039836710ccbac
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-17 16:09:05 +0800

    5-7 为 SpringBoot 增加 Interceptor 拦截器并用于记录每一个请求的日志，并注释掉了之前使用过滤器实现的日志记录

---
+++ src/main/java/io/github/yubincloud/fairywiki/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package io.github.yubincloud.fairywiki.config;
+
+import io.github.yubincloud.fairywiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+}
