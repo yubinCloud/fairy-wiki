12e016f5587c47710f59accdfc3dbd2049eff97d
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-20 16:42:31 +0800

    7-2 分类表设计与持久层代码生成 基本的无限级树型表结构设计：id + parent

---
+++ sql-scripts/create_tables.sql
@@ -0,0 +1,46 @@
+# 电子书表
+drop table if exists `ebook`;
+create table `ebook` (
+                         `id` bigint not null comment 'id',
+                         `name` varchar(50) comment '名称',
+                         `category1_id` bigint comment '分类1',
+                         `category2_id` bigint comment '分类2',
+                         `description` varchar(200) comment '描述',
+                         `cover` varchar(200) comment '封面',
+                         `doc_count` int not null default 0 comment '文档数',
+                         `view_count` int not null default 0 comment '阅读数',
+                         `vote_count` int not null default 0 comment '点赞数',
+                         primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='电子书';
+
+insert into `ebook` (id, name, description) values (1, 'Spring Boot 入门教程', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (2, 'Vue 入门教程', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
+
+
+drop table if exists `category`;
+create table `category` (
+    `id` bigint not null comment 'id',
+    `parent` bigint not null default 0 comment '父id',
+    `name` varchar(255) not null comment '名称',
+    `sort` int comment '顺序',
+    primary key (`id`)
+) engine=innodb default charset = utf8mb4 comment = '分类';
+
+insert into `category` (id, parent, name, sort) values (100, 000, '前端开发', 100);
+insert into `category` (id, parent, name, sort) values (101, 100, 'Vue', 101);
+insert into `category` (id, parent, name, sort) values (102, 100, 'HTML & CSS', 102);
+insert into `category` (id, parent, name, sort) values (200, 000, 'Java', 200);
+insert into `category` (id, parent, name, sort) values (201, 200, '基础应用', 201);
+insert into `category` (id, parent, name, sort) values (202, 200, '框架应用', 202);
+insert into `category` (id, parent, name, sort) values (300, 000, 'Python', 300);
+insert into `category` (id, parent, name, sort) values (301, 300, '基础应用', 301);
+insert into `category` (id, parent, name, sort) values (302, 300, '进阶方向应用', 302);
+insert into `category` (id, parent, name, sort) values (400, 000, '数据库', 400);
+insert into `category` (id, parent, name, sort) values (401, 400, 'MySQL', 401);
+insert into `category` (id, parent, name, sort) values (500, 000, '其它', 500);
+insert into `category` (id, parent, name, sort) values (501, 500, '服务器', 501);
+insert into `category` (id, parent, name, sort) values (502, 500, '开发工具', 502);
+insert into `category` (id, parent, name, sort) values (503, 500, '热门服务端语言', 503);
---
+++ src/main/java/io/github/yubincloud/fairywiki/mapper/CategoryMapper.java
@@ -0,0 +1,30 @@
+package io.github.yubincloud.fairywiki.mapper;
+
+import io.github.yubincloud.fairywiki.domain.Category;
+import io.github.yubincloud.fairywiki.domain.CategoryExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CategoryMapper {
+    long countByExample(CategoryExample example);
+
+    int deleteByExample(CategoryExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByExample(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
