d227b78a5d31d0d9eb5c669480f25fb271cc6588
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-22 10:40:07 +0800

    7-5 编辑父分类时改为下拉菜单

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -64,7 +64,22 @@
         <a-input v-model:value="category.name" />
       </a-form-item>
       <a-form-item label="父分类">
-        <a-input v-model:value="category.parent" />
+        <a-select
+            v-model:value="category.parent"
+            style="width: 120px"
+            @focus="focus"
+            ref="select"
+        >
+          <a-select-option value="0">
+            无
+          </a-select-option>
+          <a-select-option v-for="c in level1"
+                           :key="c.id"
+                           :value="c.id"
+                           :disabled="category.id === c.id">
+            {{ c.name }}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="category.sort" type="textarea" />
@@ -81,6 +96,13 @@ import { Tool } from "@/util/tool";
 
 interface CategoryQueryForm {
   name: string;
+}
+
+interface Category {
+  id: string;
+  name: string;
+  parent: string;
+  sort: number;
 }
 
 export default defineComponent({
@@ -160,7 +182,7 @@ export default defineComponent({
 
 
     // -------- 表单 ---------
-    const category = ref({});
+    const category = ref();
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
