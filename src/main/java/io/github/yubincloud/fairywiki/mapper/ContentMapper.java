171c98a9818d2b959e28b75c79323fdeb63c3e6d
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-23 22:13:45 +0800

    8-8 完成文档内容表的设计，生成相应的持久层代码

---
+++ src/main/java/io/github/yubincloud/fairywiki/mapper/ContentMapper.java
@@ -0,0 +1,34 @@
+package io.github.yubincloud.fairywiki.mapper;
+
+import io.github.yubincloud.fairywiki.domain.Content;
+import io.github.yubincloud.fairywiki.domain.ContentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ContentMapper {
+    long countByExample(ContentExample example);
+
+    int deleteByExample(ContentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Content record);
+
+    int insertSelective(Content record);
+
+    List<Content> selectByExampleWithBLOBs(ContentExample example);
+
+    List<Content> selectByExample(ContentExample example);
+
+    Content selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExample(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByPrimaryKeySelective(Content record);
+
+    int updateByPrimaryKeyWithBLOBs(Content record);
+}
