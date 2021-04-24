9e2fc65b1cc8ec30f4bb8ae16cf3094b8c075aa0
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-24 18:56:29 +0800

    8-12 增加文档页面，在首页点击电子书即可进入文档页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -4,12 +4,18 @@ import About from '../views/about.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
+import Doc from '../views/doc.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/doc',
+    name: 'Doc',
+    component: Doc
   },
   {
     path: '/about',
