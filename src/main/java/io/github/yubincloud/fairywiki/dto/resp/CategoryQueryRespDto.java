c55b2c9898950c60be6f04c87c5926a4da4c56e6
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-24 09:41:14 +0800

    little 给一些实体类添加了 lombok.Data 注解

--- src/main/java/io/github/yubincloud/fairywiki/dto/resp/CategoryQueryRespDto.java
+++ src/main/java/io/github/yubincloud/fairywiki/dto/resp/CategoryQueryRespDto.java
@@ -1,5 +1,8 @@
 package io.github.yubincloud.fairywiki.dto.resp;
 
+import lombok.Data;
+
+@Data
 public class CategoryQueryRespDto {
     private Long id;
 
@@ -8,50 +11,4 @@ public class CategoryQueryRespDto {
     private String name;
 
     private Integer sort;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getParent() {
-        return parent;
-    }
-
-    public void setParent(Long parent) {
-        this.parent = parent;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getSort() {
-        return sort;
-    }
-
-    public void setSort(Integer sort) {
-        this.sort = sort;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", parent=").append(parent);
-        sb.append(", name=").append(name);
-        sb.append(", sort=").append(sort);
-        sb.append("]");
-        return sb.toString();
-    }
 }
