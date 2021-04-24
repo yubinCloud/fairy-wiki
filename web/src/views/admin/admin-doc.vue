2cc7b1166eb4a665aed6713b8f0d2619d661f7a8
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-24 08:54:47 +0800

    8-9 文档管理布局美化，使表格更加紧凑，按钮变小，表单变大 页面打开时就初始化富文本框，z-index变小

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,7 +3,7 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
+      <a-row :gutter="24">
         <a-col :span="8">
           <p>
             <a-form layout="inline" :model="docQueryForm">
@@ -25,13 +25,14 @@
               :data-source="level1"
               :loading="loading"
               :pagination="false"
+              size="small"
           >
-            <template #cover="{ text: cover }">
-              <img v-if="cover" :src="cover" alt="avatar" />
+            <template #name="{ text, record }">
+              {{ record.sort }} {{ text }}
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -40,7 +41,7 @@
                     cancel-text="否"
                     @confirm="handleDeleteDoc(record.id)"
                 >
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -49,11 +50,20 @@
           </a-table>
         </a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name"/>
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSaveDoc()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
-            <a-form-item label="父文档">
+            <a-form-item>
               <a-tree-select
                   v-model:value="doc.parent"
                   style="width: 100%"
@@ -65,10 +75,10 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="顺序">
-              <a-input v-model:value="doc.sort" />
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
-            <a-form-item label="内容">
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
@@ -115,16 +125,8 @@ export default defineComponent({
     const columns = [
       {
         title: '名称',
-        dataIndex: 'name'
-      },
-      {
-        title: '父文档',
-        key: 'parent',
-        dataIndex: 'parent'
-      },
-      {
-        title: '顺序',
-        dataIndex: 'sort'
+        dataIndex: 'name',
+        slots: { customRender: 'name' }
       },
       {
         title: 'Action',
@@ -185,9 +187,8 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
-    let textEditor: E;
 
-    const handleModalOk = () => {
+    const handleSaveDoc = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
         const respData = response.data;
@@ -232,16 +233,6 @@ export default defineComponent({
       }
     };
 
-    /**
-     * 使用 textEditor 创建富文本编辑框
-     **/
-    function createTextEditor() {
-      setTimeout(function () {
-        if (!textEditor)
-          textEditor = new E('#content');
-        textEditor.create();
-      }, 300);
-    }
 
     /**
      * 编辑
@@ -256,8 +247,6 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-      // 创建富文本编辑框
-      createTextEditor();
     };
 
     /**
@@ -273,8 +262,6 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-      // 创建富文本编辑框
-      createTextEditor();
     }
 
     let docIdListToDelete: Array<string> = [];
@@ -342,6 +329,9 @@ export default defineComponent({
 
     onMounted(() => {
       handleQuery();
+      const textEditor = new E('#content');
+      textEditor.config.zIndex = 0;
+      textEditor.create();
     });
 
     return {
@@ -358,7 +348,7 @@ export default defineComponent({
       add,
       handleDeleteDoc,
       handleQueryFormSubmit,
-      handleModalOk,
+      handleSaveDoc,
 
 
       doc,
