bf287ac9a3c67a8074e88b7f188d6355b5cfdcbf
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-24 09:05:03 +0800

    8-9 让表格树默认展开，利用给组件加一个 v-if 判断使得当真正有数据时才允许 Ant Design 进行渲染，防止初始因为数据为空而渲染失败

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -20,12 +20,14 @@
             </a-form>
           </p>
           <a-table
+              v-if="level1.length > 0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
               size="small"
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text, record }">
               {{ record.sort }} {{ text }}
@@ -147,6 +149,7 @@ export default defineComponent({
      * }]
      */
     const level1 = ref(); // 一级文档树，children属性就是二级文档
+    level1.value = [];
 
 
     /**
