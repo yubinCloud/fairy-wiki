609f7e220fbb3c51b2dacbb037a1bc6032f0a399
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-24 19:34:49 +0800

    8-12 使用树形组件展示文档树 遗留BUG：文档树应该只查当前电子书的文档

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -1,9 +1,76 @@
 <template>
   <a-layout>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-      <div class="doc">
-        <h1>欢迎来到文档页面</h1>
-      </div>
+      <a-row>
+        <a-col :span="6">
+          <a-tree
+              v-if="level1.length > 0"
+              :tree-data="level1"
+              @select="onSelect"
+              :replaceFields="{title: 'name', key: 'id', value: 'id'}"
+              :defaultExpandAll="true"
+          >
+          </a-tree>
+        </a-col>
+        <a-col :span="18">
+        </a-col>
+      </a-row>
     </a-layout-content>
   </a-layout>
 </template>
+
+
+<script lang="ts">
+import { defineComponent, onMounted, ref} from 'vue';
+import axios from 'axios';
+import {message} from 'ant-design-vue';
+import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
+
+export default defineComponent({
+  name: 'AdminDoc',
+  setup() {
+    const route = useRoute();
+    const docs = ref();
+
+    /**
+     * 一级文档树，children属性就是二级文档
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // 一级文档树，children属性就是二级文档
+    level1.value = [];
+
+    /**
+     * 数据查询
+     **/
+    const handleQueryDoc = () => {
+      axios.get("/doc/all").then((response) => {
+        const respData = response.data;
+        if (respData.code === 0) {
+          docs.value = respData.data;
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(docs.value, 0);
+        } else {
+          message.error(respData.message);
+        }
+      });
+    };
+
+    onMounted(() => {
+      handleQueryDoc();
+    });
+
+    return {
+      level1,
+    }
+  }
+});
+</script>
