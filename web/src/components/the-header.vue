935b78d8f96971884cd88f811f06ad214c633da6
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-25 19:00:46 +0800

    9-8 前端增加登录框

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -4,7 +4,6 @@
     <a-menu
         theme="dark"
         mode="horizontal"
-        v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
     >
       <a-menu-item key="/">
@@ -22,15 +21,65 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+      <a class="login-menu" @click="showLoginModal">
+        <span>登录</span>
+      </a>
     </a-menu>
+
+    <a-modal
+        title="登录"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login"
+    >
+      <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginUser.loginName" />
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password" type="password" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'the-header'
+  name: 'the-header',
+  setup () {
+    const loginUser = ref({
+      loginName: "test",
+      password: "test"
+    });
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
+    };
+
+    // 登录
+    const login = () => {
+      console.log("开始登录")
+    };
+
+    return {
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      loginUser,
+      login
+    }
+  }
 })
 
 </script>
+
+<style>
+.login-menu {
+  float: right;
+  color: white;
+}
+</style>
