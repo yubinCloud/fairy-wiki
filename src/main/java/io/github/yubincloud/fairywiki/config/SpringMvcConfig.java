971e739830c2b7a2de08eec3e11a8a0eaf89bbcd
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-27 15:19:32 +0800

    9-11 前后端加入请求拦截用于验证 token

--- src/main/java/io/github/yubincloud/fairywiki/config/SpringMvcConfig.java
+++ src/main/java/io/github/yubincloud/fairywiki/config/SpringMvcConfig.java
@@ -1,14 +1,29 @@
 package io.github.yubincloud.fairywiki.config;
 
+import io.github.yubincloud.fairywiki.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-    public void addInterceptors(InterceptorRegistry registry) {
+    @Resource
+    LoginInterceptor loginInterceptor;
 
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/query",
+                        "/doc/all/**",
+                        "/doc/read-content/**"
+                );
     }
 }
