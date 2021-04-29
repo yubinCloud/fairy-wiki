5a367a6c22cbab1aedee369f6a39b53c2ac63614
yubinCloud <yubin_SkyWalker@yeah.net>
2021-04-29 11:17:52 +0800

    10-7 增加日志流水号，方便运维管理

--- src/main/java/io/github/yubincloud/fairywiki/aspect/LogAspect.java
+++ src/main/java/io/github/yubincloud/fairywiki/aspect/LogAspect.java
@@ -3,6 +3,7 @@ package io.github.yubincloud.fairywiki.aspect;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 import io.github.yubincloud.fairywiki.utils.RequestContext;
+import io.github.yubincloud.fairywiki.utils.SnowFlake;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
@@ -12,11 +13,13 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +30,9 @@ public class LogAspect {
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /** 定义一个切点 */
     @Pointcut("execution(public * io.github.yubincloud.fairywiki.controller..*Controller.*(..))")
     public void controllerPointcut() {}
@@ -34,6 +40,9 @@ public class LogAspect {
 
     @Before("controllerPointcut()")
     public void doBefore(JoinPoint joinPoint) {
+
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
 
         // 开始打印请求日志
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
--- src/main/java/io/github/yubincloud/fairywiki/job/DocJob.java
+++ src/main/java/io/github/yubincloud/fairywiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package io.github.yubincloud.fairywiki.job;
 
 import io.github.yubincloud.fairywiki.service.DocService;
+import io.github.yubincloud.fairywiki.utils.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +18,16 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 每30秒更新电子书信息
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新电子书下的文档数据开始");
         long start = System.currentTimeMillis();
         docService.updateEbookFooter();
